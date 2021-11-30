1645ce418ac96214d8615f59be76580ed5b3c402
Marcos Eduardo <markuzemb18@gmail.com>
2021-11-29 23:19:55 -0600

    Aplicacion Gif Expert

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,14 @@
+export const getGifs = async (category) => {
+    const URL = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=ZU0ogO0xJrPUCCqZ7q8Pdt9Y6qRo3251`;
+    const resp = await fetch(URL);
+    const { data } = await resp.json();
+
+    const gifs = data.map(img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    });
+    return gifs;
+}
